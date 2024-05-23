01b1108fb191b29744e20cadc4b22bd57a2276b1
muyeon <gmakin36@gmail.com>
2024-05-23 17:34:07 +0900

    style: add global style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    border: 1px solid black;
+    width: 60%;
+    height: auto;
+    margin: 0 auto;
+  }
+`;
+
+export default GlobalStyle;
